54b90b558734006a9eec50ac4aad7ee8ddea86f0
behnass <abderrahmane.behnass@econocom.com>
2018-08-10 20:19:47 +0200

    Last change V2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
+            new RestaurantCornerBundle\RestaurantCornerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
